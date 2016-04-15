547cb09dc18d30da33997a65e6479ce801eccde4
Alexandr <Alexandr@macbook-pro-alexandr.local>
2016-04-15 16:50:45 +0300

    Initial Commit

---
+++ TestTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTask
+//
+//  Created by Alexandr on 15.04.16.
+//  Copyright © 2016 Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
